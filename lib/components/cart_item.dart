81a9aea51c66b12d4cb7558ef4678d85f17e4990
Mateus Felipe Ribeiro <mateus.ribeiro@afixcode.com.br>
2025-10-06 17:34:32 -0300

    att2

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -27,6 +27,29 @@ class CartItemWidget extends StatelessWidget {
           size: 40,
         ),
       ),
+      confirmDismiss: (_) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Tem certeza?'),
+            content: Text("Quer remover o item do carrinho?"),
+            actions: [
+              TextButton(
+                child: Text('Não'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              TextButton(
+                child: Text('Sim'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (_) {
         Provider.of<Cart>(
           context,
